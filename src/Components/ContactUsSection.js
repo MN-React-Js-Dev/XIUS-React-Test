ba8b520601c1ef4eba6686357f3b2b7fdcc76675
mn-react-js-dev <mayur.nandwalker.5057@gmail.com>
2023-09-20 14:50:54 +0530

    button padding chnage

--- src/Components/ContactUsSection.js
+++ src/Components/ContactUsSection.js
@@ -5,9 +5,9 @@ const ContactUsSection = () => {
     <>
     <section className='contact-us-section'>
         <div className='container'> 
-        <div className='col-md-6 pt-5 pb-5'>
+        <div className='col-md-6 pt-5 pb-5 center-content-mobile'>
             <h1 className='text-start are-you-ready'>Are You Ready to Deliver Rich, Real-time Communication with XIUS?</h1>
-            <button className='btn btn-outline contact-us-button p-2 px-4 mt-4'>Contact us now</button>
+            <button className='btn btn-outline contact-us-button p-3 px-4 mt-4'>Contact us now</button>
         </div>
         </div>
     </section>
