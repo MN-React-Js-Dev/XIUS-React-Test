e906b2517c7b5e3147560f29d3b1189909b2cd7b
mn-react-js-dev <mayur.nandwalker.5057@gmail.com>
2023-09-21 11:15:38 +0530

    test

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import RequestRecive from "./RequestRecive";
 import LetsTalkComponent from "./LetsTalkComponent";
+import HeroImage1 from '../assets/Images/heroimage.png'
+import HeroImage2 from '../assets/Images/heroSecindImage.png'
 
 const HeroSection = () => {
   const [flag, setflag] = useState(false)
@@ -8,170 +10,178 @@ const HeroSection = () => {
   const handleSubmitButton = () => {
     setflag(true)
   }
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const carousel = document.getElementById("carouselExampleIndicators");
-      const currentIndex = parseInt(carousel.dataset.slideTo, 10) || 0;
-      const nextIndex = currentIndex + 1 >= 2 ? 0 : currentIndex + 1;
-      carousel.dataset.slideTo = nextIndex;
-      carousel.querySelector(`[data-bs-slide-to="${nextIndex}"]`).click();
-    }, 2500);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+  const headings = [
+    "Revolutionize Your Communication with IP Multimedia Subsystem (IMS)",
+    "Unlock the Power of IP Multimedia Subsystem with XIUS IMS"
+  ];
+
+  const paras = [
+    "XIUS IMS Suite – an open, carrier-grade, fully virtualized and cloud-ready solution for fixed, mobile, and converged service providers.",
+    "Take Your Communication Infrastructure to the Next Level with IMS"
+  ];
+
+  const HeroImages = [HeroImage1, HeroImage2]
+
+  let currentIndex = 0;
+  let currentIndexPara = 0;
+  let currentIndexHeroImages = 0;
+
+  function changeHeading() {
+    const headingElement = document.querySelector(".hero-heading");
+    const parasElement = document.querySelector(".para-hero");
+    headingElement.textContent = headings[currentIndex];
+    parasElement.textContent = headings[currentIndex];
+    currentIndex = (currentIndex + 1) % headings.length;
+
+    const paraElement = document.querySelector(".para-hero");
+    paraElement.textContent = paras[currentIndexPara];
+    currentIndexPara = (currentIndexPara + 1) % paras.length;
+
+    const textColor = currentIndex % 2 === 0 ? "black" : "white";
+    headingElement.style.color = textColor;
+    parasElement.style.color = textColor;
+    // Change background image
+    const backgroundElement = document.querySelector(".hero-section");
+    backgroundElement.style.backgroundImage = `url(${HeroImages[currentIndexHeroImages]})`;
+    currentIndexHeroImages = (currentIndexHeroImages + 1) % HeroImages.length;
+}
+
+  setInterval(changeHeading, 3000); 
+
 
   return (
     <>
 
-      <div id="carouselExampleIndicators" class="carousel slide">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <section className="hero-section d-none d-lg-block d-print-block">
-              <div className="containers cst-container-hero-section ">
-                <div className="row">
-                  <div className="col-sm-6  ">
-                    <h1 className="hero-heading mt-5">
-                      Revolutionize Your Communication with IP Multimedia Subsystem (IMS)
-                    </h1>
-                    <p className="para-hero">
-                      XIUS IMS Suite – an open, carrier-grade, fully virtualized and
-                      cloud-ready solution for fixed, mobile, and converged service providers.
-                    </p>
-                    <button className=" mb-5 btn btn-outline text-danger talk-to-expert-btn p-3 px-5">Talk to our experts</button>
-                  </div>
-                  <div className="col-sm-6 second-column-hero">
-                    {flag ? <RequestRecive /> : <>
-                      <div className="container card cst-hero-card ">
-                        <div className="row">
-                          <h1 className="text-center "><b>Let’s talk</b></h1>
-                          <div className="col-sm-6">
-                            <div className="inpt-wrap ">
-                              <label>Full name</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap ">
-                              <label>Work email ID</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap ">
-                              <label>Mobile number</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap ">
-                              <label>Country</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap">
-                              <label>Designation</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                          </div>
-                          <div className="col-sm-6">
-                            <div className="inpt-wrap ">
-                              <label>Company name</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap ">
-                              <label>Industry</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap ">
-                              <label>Message</label>
-                              <textarea className="form-control" rows={8} placeholder="Enter here"></textarea>
-                            </div>
-                          </div>
-                          <div className="d-flex justify-content-center ">
-                            <button className="btn-success form-control cst-hero-submit-btn w-50 " onClick={handleSubmitButton}>Submit</button>
-                          </div>
-                        </div>
+
+      <section className="hero-section d-none d-lg-block d-print-block">
+        <div className="containers cst-container-hero-section ">
+          <div className="row">
+            <div className="col-sm-6  ">
+              <h1 className="hero-heading mt-5"  >
+                Revolutionize Your Communication with IP Multimedia Subsystem (IMS)
+              </h1>
+              <p className="para-hero">
+                XIUS IMS Suite – an open, carrier-grade, fully virtualized and
+                cloud-ready solution for fixed, mobile, and converged service providers.
+              </p>
+              <button className=" mb-5 btn btn-outline text-danger talk-to-expert-btn p-3 px-5">Talk to our experts</button>
+            </div>
+            <div className="col-sm-6 second-column-hero">
+              {flag ? <RequestRecive /> : <>
+                <div className="container card cst-hero-card ">
+                  <div className="row">
+                    <h1 className="text-center "><b>Let’s talk</b></h1>
+                    <div className="col-sm-6">
+                      <div className="inpt-wrap ">
+                        <label>Full name</label>
+                        <input className="form-control" placeholder="Enter here" />
                       </div>
-                    </>}
+                      <div className="inpt-wrap ">
+                        <label>Work email ID</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap ">
+                        <label>Mobile number</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap ">
+                        <label>Country</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap">
+                        <label>Designation</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                      <div className="inpt-wrap ">
+                        <label>Company name</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap ">
+                        <label>Industry</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap ">
+                        <label>Message</label>
+                        <textarea className="form-control" rows={8} placeholder="Enter here"></textarea>
+                      </div>
+                    </div>
+                    <div className="d-flex justify-content-center ">
+                      <button className="btn-success form-control cst-hero-submit-btn w-50 " onClick={handleSubmitButton}>Submit</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </section>
-          </div>
-          <div class="carousel-item">
-            <section className="hero-section-two d-none d-lg-block d-print-block">
-              <div className="containers cst-container-hero-section ">
-                <div className="row">
-                  <div className="col-sm-6">
-                    <h1 className="hero-heading mt-5">
-                      Unlock the Power of IP Multimedia Subsystem with XIUS IMS
-                    </h1>
-                    <p className="para-hero mt-2">
-                      Take Your Communication Infrastructure to the Next Level with IMS
-                    </p>
-                    <button className="mb-5 btn btn-outline text-danger talk-to-expert-btn p-3 px-5">Talk to our experts</button>
-                  </div>
-                  <div className="col-sm-6 second-column-hero">
-                    {flag ? <RequestRecive /> : <>
-                      <div className="container card cst-hero-card ">
-                        <div className="row">
-                          <h1 className="text-center "><b>Let’s talk</b></h1>
-                          <div className="col-sm-6">
-                            <div className="inpt-wrap ">
-                              <label>Full name</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap ">
-                              <label>Work email ID</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap ">
-                              <label>Mobile number</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap ">
-                              <label>Country</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap">
-                              <label>Designation</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                          </div>
-                          <div className="col-sm-6">
-                            <div className="inpt-wrap ">
-                              <label>Company name</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap ">
-                              <label>Industry</label>
-                              <input className="form-control" placeholder="Enter here" />
-                            </div>
-                            <div className="inpt-wrap ">
-                              <label>Message</label>
-                              <textarea className="form-control" rows={8} placeholder="Enter here"></textarea>
-                            </div>
-                          </div>
-                          <div className="d-flex justify-content-center ">
-                            <button className="btn-success form-control cst-hero-submit-btn w-50 " onClick={handleSubmitButton}>Submit</button>
-                          </div>
-                        </div>
-                      </div>
-                    </>}
-                  </div>
-                </div>
-              </div>
-            </section>
+              </>}
+            </div>
           </div>
         </div>
-        {/* <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button> */}
-      </div>
+      </section>
+      {/* <section className="hero-section-two d-none d-lg-block d-print-block">
+        <div className="containers cst-container-hero-section ">
+          <div className="row">
+            <div className="col-sm-6">
+              <h1 className="hero-heading mt-5">
+                Unlock the Power of IP Multimedia Subsystem with XIUS IMS
+              </h1>
+              <p className="para-hero mt-2">
+                Take Your Communication Infrastructure to the Next Level with IMS
+              </p>
+              <button className="mb-5 btn btn-outline text-danger talk-to-expert-btn p-3 px-5">Talk to our experts</button>
+            </div>
+            <div className="col-sm-6 second-column-hero">
+              {flag ? <RequestRecive /> : <>
+                <div className="container card cst-hero-card ">
+                  <div className="row">
+                    <h1 className="text-center "><b>Let’s talk</b></h1>
+                    <div className="col-sm-6">
+                      <div className="inpt-wrap ">
+                        <label>Full name</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap ">
+                        <label>Work email ID</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap ">
+                        <label>Mobile number</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap ">
+                        <label>Country</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap">
+                        <label>Designation</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                      <div className="inpt-wrap ">
+                        <label>Company name</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap ">
+                        <label>Industry</label>
+                        <input className="form-control" placeholder="Enter here" />
+                      </div>
+                      <div className="inpt-wrap ">
+                        <label>Message</label>
+                        <textarea className="form-control" rows={8} placeholder="Enter here"></textarea>
+                      </div>
+                    </div>
+                    <div className="d-flex justify-content-center ">
+                      <button className="btn-success form-control cst-hero-submit-btn w-50 " onClick={handleSubmitButton}>Submit</button>
+                    </div>
+                  </div>
+                </div>
+              </>}
+            </div>
+          </div>
+        </div>
+      </section> */}
+
 
 
 
@@ -186,8 +196,8 @@ const HeroSection = () => {
           </div>
         </div>
       </section>
-      <div className="d-flex justify-content-center mt-3 d-sm-none">
-        <button className="btn-success col-10 cst-hero-submit-btn p-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Contact us</button>
+      <div className="d-flex justify-content-center d-sm-none" style={{ backgroundColor: "white" }}>
+        <button className="btn-success col-10 cst-hero-submit-btn p-3 scrollable-button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Contact us</button>
       </div>
 
       <LetsTalkComponent />
